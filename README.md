9aff84c6fee99dc7ead57f2e5baee4524976d6a8
Mehul Kiran Chaudhari <chaudharimehul2001@gmail.com>
2021-10-16 11:13:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# contacts
+ This repository has both front-end and backend of the contacts app. 
---
+++ contacts-client/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App(): JSX.Element {
+  return <h1>Contacts</h1>;
+}
+
+export default App;
