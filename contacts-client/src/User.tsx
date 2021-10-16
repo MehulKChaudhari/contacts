40257bc4d4ebc5be95a1b3a3d6806aaa0b0811f3
Mehul Kiran Chaudhari <chaudharimehul2001@gmail.com>
2021-10-16 16:24:58 +0530

    added: user details page

--- contacts-client/src/User.tsx
+++ contacts-client/src/User.tsx
@@ -1,17 +1,17 @@
 import {
   Box,
+  Button,
   Flex,
+  Stack,
   Table,
-  Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
-  TableCaption,
 } from "@chakra-ui/react";
 import React from "react";
-import { FaUserAlt } from "react-icons/fa";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { BsFillPencilFill } from "react-icons/bs";
 
 const User = (): JSX.Element => {
   return (
@@ -31,22 +31,57 @@ const User = (): JSX.Element => {
           height={["", "500px"]}
           alignContent="center"
         >
-          <Table variant="simple" alignSelf="center">
-            <Tbody>
-              <Tr>
-                <Td>inches</Td>
-                <Td>millimetres (mm)</Td>
-              </Tr>
-              <Tr>
-                <Td>feet</Td>
-                <Td>centimetres (cm)</Td>
-              </Tr>
-              <Tr>
-                <Td>yards</Td>
-                <Td>metres (m)</Td>
-              </Tr>
-            </Tbody>
-          </Table>
+          <Stack spacing={5}>
+            <Flex>
+              <AiOutlineArrowLeft size="2rem" />
+            </Flex>
+            <Flex>
+              <Table variant="simple">
+                <Tbody fontSize="1.1rem">
+                  <Tr>
+                    <Th outline="none" border="none">
+                      FirstName:
+                    </Th>
+                    <Td outline="none" border="none">
+                      Mehul
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Th outline="none" border="none">
+                      LastName:
+                    </Th>
+                    <Td outline="none" border="none">
+                      Chaudhari
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Th outline="none" border="none">
+                      Email:
+                    </Th>
+                    <Td outline="none" border="none">
+                      MehulChaduahri@gmail.com
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Th outline="none" border="none">
+                      Contact Number:
+                    </Th>
+                    <Td outline="none" border="none">
+                      9184858281
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </Flex>
+            <Flex justifyContent="space-between" p={4}>
+              <Button colorScheme="red" variant="outline">
+                Delete
+              </Button>
+              <Button colorScheme="teal" variant="solid">
+                <BsFillPencilFill /> Edit
+              </Button>
+            </Flex>
+          </Stack>
         </Box>
       </Flex>
     </>
